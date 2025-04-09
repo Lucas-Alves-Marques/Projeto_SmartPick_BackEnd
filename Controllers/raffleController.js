a40040808c689051bc39916680e112b0198ef1b9
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-04-09 20:49:57 -0300

    Estruturação do BackEnd

--- Controllers/raffleController.js
+++ Controllers/raffleController.js
@@ -3,7 +3,29 @@ import service from "../Services/raffleService.js";
 
 const route = express.Router();
 
-route.get("/raflle", async (request, response) => {
+route.get("/raffle", async (request, response) => {
 
-    
+
+})
+
+route.post("/createRaffle", async (request, response) => {
+
+    const [id_user, name, item] = request.body
+
+    try{
+
+        await
+        
+        await
+
+        item.forEach(a => {
+
+            await
+
+        })
+
+
+    }catch(err){
+
+    }
 })
--- Service/raffleService.js
+++ Service/raffleService.js
@@ -2,7 +2,7 @@ import database from '../Repository/mysql.js';
 
 async function listRaffle(id_raffle) {
 
-    const sql = "SELECT * FROM raffle WHERE id_raffle = ?";
+    const sql = "SELECT raffle.name AS raffle, category.title AS category, items.name AS item FROM items INNER JOIN category ON category.id_category = items.id_category INNER JOIN raffle ON raffle.id_raffle = category.id_raffle";
 
     const conn = await database.connectDB();
 
@@ -12,6 +12,16 @@ async function listRaffle(id_raffle) {
 
     return rows;
 
+}
+
+async function createRaffle(id_user, name) {
+
+    const SQL = 'insert into raffle(id_user, name) values(? , ?);'
+
+    const conn = database.connectDB()
+
+    await conn.query(SQL, [id_user, name])
+    
 }
 
 export default{listRaffle}
