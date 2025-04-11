0a12c23b64b33c9e90b1bb4f22e48af6efbcbd44
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-04-10 22:32:54 -0300

    Adição do Cors

--- Index.js
+++ Index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import routes from "./Routes.js";
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", routes);
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
+
 });
